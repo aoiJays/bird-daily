fa8a66f3b8536a083b292f4bffce64b878e8dfe6
aoijays <neworld1111@163.com>
2026-01-14 02:07:26 +0800

    first commit

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def run_scraper():
         driver = setup_driver()
         
         # 访问知乎的一个具体问题页面（比首页更容易访问，首页往往强制登录）
-        url = "https://www.zhihu.com/explore" 
+        url = "https://www.zhihu.com/people/xule.null/posts" 
         print(f"正在访问: {url}")
         
         driver.get(url)
